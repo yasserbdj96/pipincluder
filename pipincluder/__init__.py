4cb32d506650be853f3b2be488761be7fa037711
Yasser BDJ (Ro0t-96) <by.root96@gmail.com>
2021-05-15 17:46:47 +0100

    Delete pipincluder directory

--- pipincluder/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding:utf-8
-# code by : Yasser BDJ
-# email : by.root96@gmail.com
-#s
-from pipincluder.__version__ import __version__
-from pipincluder.pipincluder import pipincluder
-#e
